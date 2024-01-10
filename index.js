d9ee870adb048ec44d2cf2db9de3d08b10dbe1b9
Juan <114969408+Vn0m@users.noreply.github.com>
2024-01-10 07:54:25 -0500

    add fetch to weather (#5)
    * add card element to the page
    
    * add files and start making card style
    
    * card style finished
    
    * fix button position
    
    * fetch weather api

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const button = document.getElementById("slider")
 button.onclick = function(){
     document.body.classList.toggle("light-theme");
     if (document.body.classList.contains("light-theme")){
-        icon.src = "icons/sun.png";
+        icon.src = "public/sun.png";
     } else {
-        icon.src = "icons/moon.png";
+        icon.src = "public/moon.png";
     }
 }
+
+API_KEY = ""
+API_URL = "http://api.weatherapi.com/v1"
+defaultLocation = "New York"
+
+
+
+    // Make the HTTP request 
+    fetch(`${API_URL}/current.json?key=${API_KEY}&q=${defaultLocation}`)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data here
+        console.log('Global Weather Information:', data);
+      })
+      .catch(error => {
+        // Handle error
+        console.error('Error fetching global weather data:', error);
+      });
+
