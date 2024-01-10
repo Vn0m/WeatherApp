ee498f7a78e37480d048947bd23edf9fd5996ef1
Vn0m <juanpapolo123@gmail.com>
2024-01-09 23:39:18 -0500

    fetch weather api

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ button.onclick = function(){
         icon.src = "public/moon.png";
     }
 }
+
+API_KEY = ""
+API_URL = "http://api.weatherapi.com/v1"
+defaultLocation = "New York"
+
+
+
+    // Make the HTTP request 
+    fetch(`${API_URL}/current.json?key=${API_KEY}&q=${defaultLocation}`)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data here
+        console.log('Global Weather Information:', data);
+      })
+      .catch(error => {
+        // Handle error
+        console.error('Error fetching global weather data:', error);
+      });
+
